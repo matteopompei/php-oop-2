a8e69f28dbe3e1c96e01ef8fa4230fee9e5d4555
Matteo Pompei <datxizzan@gmail.com>
2022-02-28 16:50:50 +0100

    File PHP creati ed inclusi nell'index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-oop-2</title>
+</head>
+<body>
+<?php
+  require_once __DIR__."./classes/Film.php";
+  require_once __DIR__."./classes/Sala.php";
+  require_once __DIR__."./classes/Sala4D.php";
+  require_once __DIR__."./classes/Spettacoli.php";
+
+?>
+</body>
+</html>
