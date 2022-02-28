85227e8a5408f5a025da9da9ba8ce533eebd7cb5
Matteo Pompei <datxizzan@gmail.com>
2022-02-28 17:11:33 +0100

    Film abbozzato

--- classes/Film.php
+++ classes/Film.php
@@ -1,1 +1,19 @@
 <?php
+
+  class Film {
+    protected $titolo;
+    protected $genere;
+    protected $durata;
+    protected $trama;
+    protected $regista;
+    protected $cast;
+
+    public function __construct($_titolo, $_genere, $_durata, $_trama, $_regista, $_cast) {
+      $this->titolo = $_titolo;
+      $this->genere = $_genere;
+      $this->durata = $_durata;
+      $this->trama = $_trama;
+      $this->regista = $_regista;
+      $this->cast = $_cast;
+    }
+  }
