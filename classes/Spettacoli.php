9cc58548c1b0077af190da62b412865d3997a24c
Matteo Pompei <datxizzan@gmail.com>
2022-02-28 17:14:35 +0100

    Spettacoli abbozzati

--- classes/Spettacoli.php
+++ classes/Spettacoli.php
@@ -1,1 +1,15 @@
 <?php
+  class Spettacoli {
+
+    protected $film;
+    protected $date;
+    protected $orari;
+    protected $sala;
+    
+    public function __construct() {
+      this->film = $_film;
+      this->date = $_date;
+      this->orari = $_orari;
+      this->sala = $_sala;
+    }
+  }
