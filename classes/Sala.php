c6ee88a030f80a4299e9adb1ea70efaa077101d0
Matteo Pompei <datxizzan@gmail.com>
2022-02-28 17:06:59 +0100

    Sale abbozzate

--- classes/Sala.php
+++ classes/Sala.php
@@ -1,1 +1,15 @@
 <?php
+
+  class Sala {
+    protected $id;
+    protected $capienza;
+    
+    public function __construct($_numero, $_capienza) {
+      $this->id = $_numero;
+      $this->capienza = $_capienza;
+    }
+
+    public function getInfo() {
+      return "{$this->id} {$this->capienza}";
+    }
+  }
--- classes/Sala4D.php
+++ classes/Sala4D.php
@@ -1,1 +1,15 @@
 <?php
+
+  class Sala4D extends Sala {
+    protected $vibrazione;
+    protected $fumo;
+    protected $acqua;
+
+    public function __construct($_numero, $_capienza, $_vibrazione, $_fumo, $_acqua) {
+      $this->id = $_numero;
+      $this->capienza = $_capienza;
+      $this->vibrazione = $_vibrazione;
+      $this->fumo = $_fumo;
+      $this->acqua = $_acqua;
+    }
+  }
