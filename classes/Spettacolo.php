545db1ed3c269b54407d01e5b5d5dc063535b923
Matteo Pompei <datxizzan@gmail.com>
2022-03-01 16:00:58 +0100

    Cambio  nomi

---
+++ classes/Spettacolo.php
@@ -0,0 +1,15 @@
+<?php
+
+  class Spettacolo {
+    protected $film;
+    protected $date;
+    protected $orari;
+    protected $sala;
+    
+    public function __construct($_film, $_date, $_orari, $_sala) {
+      $this->film = $_film;
+      $this->date = $_date;
+      $this->orari = $_orari;
+      $this->sala = $_sala;
+    }
+  }
